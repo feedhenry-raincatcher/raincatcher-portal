2bfa96a5d6f7cde591e66f37e7262f9929084d56
Austin Cunningham <aucunnin@redhat.com>
2017-11-27 11:54:00 +0000

    update hostname

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,7 +140,7 @@ module.exports = function(grunt) {
         port: 8080,
         // Change this to '0.0.0.0' to access the server from outside.
         //hostname: 'localhost',
-        hostname: 'raincatcher-portal-raincatcher.127.0.0.1.nip.io',
+        hostname: '0.0.0.0',
         livereload: 35731
       },
       livereload: {
